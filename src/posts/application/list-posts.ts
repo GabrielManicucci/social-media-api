258d738e6e8e9d105bdd83fce8fc34404f48f908
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2026-04-30 12:04:27 -0300

    feat: add user details of who liked a post in list posts...

--- src/posts/application/list-posts.ts
+++ src/posts/application/list-posts.ts
@@ -1,10 +1,10 @@
 import { IPostsRepository } from "../domain/posts.interface.repository";
-import { PostWithAuthor } from "../domain/posts.dto.response";
+import { PostWithDetails } from "../domain/posts.dto.response";
 
 export class ListPostsUseCase {
   constructor(private readonly postsRepository: IPostsRepository) {}
 
-  async execute(): Promise<PostWithAuthor[]> {
+  async execute(): Promise<PostWithDetails[]> {
     return await this.postsRepository.findAll();
   }
 }
--- src/posts/application/list-top-posts.ts
+++ src/posts/application/list-top-posts.ts
@@ -1,10 +1,10 @@
 import { IPostsRepository } from "../domain/posts.interface.repository";
-import { PostWithAuthor } from "../domain/posts.dto.response";
+import { PostWithDetails } from "../domain/posts.dto.response";
 
 export class ListTopLikedPostsUseCase {
   constructor(private readonly postsRepository: IPostsRepository) {}
 
-  async execute(limit: number = 10): Promise<PostWithAuthor[]> {
+  async execute(limit: number = 10): Promise<PostWithDetails[]> {
     return await this.postsRepository.findTopLiked(limit);
   }
 }
